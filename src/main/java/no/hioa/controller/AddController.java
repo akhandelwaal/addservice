2c9c3a0d0d1a624d4f39ab2debecb19ddc9d4aa0
Ankit Khandelwal <khandelwaal.ankit@gmail.com>
2018-03-04 20:08:37 +0530

    2nd commit

--- src/main/java/no/hioa/controller/AddController.java
+++ src/main/java/no/hioa/controller/AddController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by Ankit and Dhaval 03032018
+ * Created by Usman on 08.05.2017.
  */
 
 @RestController
